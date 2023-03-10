aed59ac3fedc20914601c3e6b1894020e7ceb5e5
Lois KOUNINEF <lkouninef@gmail.com>
2023-03-10 17:44:25 +0100

    add instance test

--- src/app/core/services/api-call/api-call.service.spec.ts
+++ src/app/core/services/api-call/api-call.service.spec.ts
@@ -26,17 +26,21 @@ describe('ApiCallService', () => {
 
   it('should have a get method', () => {
     expect(service.get<any>).toBeDefined();
+    expect(service.get<any>).toBeInstanceOf(Function);
   });
 
   it('should have a post method', () => {
     expect(service.post<any>).toBeDefined();
+    expect(service.post<any>).toBeInstanceOf(Function);
   });
 
   it('should have a patch method', () => {
     expect(service.patch<any>).toBeDefined();
+    expect(service.patch<any>).toBeInstanceOf(Function);
   });
 
   it('should have a delete method', () => {
     expect(service.delete<any>).toBeDefined();
+    expect(service.delete<any>).toBeInstanceOf(Function);
   });
 });
