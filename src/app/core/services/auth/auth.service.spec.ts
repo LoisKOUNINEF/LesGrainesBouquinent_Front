026eeee8178d52a64ab048b2d862a6494066c74a
Lois KOUNINEF <lkouninef@gmail.com>
2023-03-14 14:20:02 +0100

    setyp for tests

---
+++ src/app/core/services/auth/auth.service.spec.ts
@@ -0,0 +1,31 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { createMock } from '@golevelup/ts-jest';
+import { ApiCallService } from '../api-call/api-call.service';
+
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+      {
+        provide: ApiCallService,
+        useValue: createMock<ApiCallService>(),
+      }
+      ],
+    });
+    service = TestBed.inject(AuthService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
