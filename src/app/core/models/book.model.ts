71ee4443ae9af4bf392c51b3e27e634bd4665fdc
Lois KOUNINEF <lkouninef@gmail.com>
2023-03-15 15:27:14 +0100

    book & comment model

--- src/app/core/models/book.model.ts
+++ src/app/core/models/book.model.ts
@@ -1,9 +1,29 @@
+import { Comment } from "./comment.model";
 import { BaseModel } from "./shared/base.model";
+import { User } from "./user.model";
 
 export class Book extends BaseModel {
+	title: string;
+	author: string;
+	description: string;
+	picture: URL;
+	comments: Comment[];
+	user: User;
 
 	constructor(
+		title: string,
+		author: string,
+		description: string,
+		picture: URL,
+		comments: Comment[] = [],
+		user: User,
 	) {
     super();
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.picture = picture;
+    this.comments = comments;
+    this.user = user;
   }
 }
--- src/app/core/models/comment.model.ts
+++ src/app/core/models/comment.model.ts
@@ -1,9 +1,20 @@
+import { Book } from "./book.model";
 import { BaseModel } from "./shared/base.model";
+import { User } from "./user.model";
 
 export class Comment extends BaseModel {
+	content: string;
+	user: User;
+	book: Book;
 
 	constructor(
+		content: string,
+		user: User,
+		book: Book,
 	) {
     super();
+    this.content = content;
+    this.user = user;
+    this.book = book;
   }
 }
