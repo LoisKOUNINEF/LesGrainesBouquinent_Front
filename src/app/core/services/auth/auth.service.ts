9440f605ad15dfec0621acd42c53e7dc65696dc3
Lois KOUNINEF <lkouninef@gmail.com>
2023-03-16 12:28:24 +0100

    login method

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable, filter, map } from 'rxjs';
+import { UserDTO } from '../../dto/user.dto';
+import { Result } from '../../models/shared/result.model';
+import { User } from '../../models/user.model';
 import { ApiCallService } from '../api-call/api-call.service';
 
 @Injectable({
@@ -7,4 +11,15 @@ import { ApiCallService } from '../api-call/api-call.service';
 export class AuthService {
 
   constructor( private apiCallService: ApiCallService ) { }
+
+  public login(userDto: UserDTO): Observable<User> {
+    return this.apiCallService
+      .post<User>('/users/login', userDto)
+      .pipe(
+      filter(res => !!res),
+      map((response: any) => {
+        return response;
+      })
+    );
+  }
 }
