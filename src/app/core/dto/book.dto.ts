aa795aa2b94428f6c163a0c8e7d1e7071e6bba8e
Lois KOUNINEF <lkouninef@gmail.com>
2023-03-16 08:58:36 +0100

    book & comment dtos

--- src/app/core/dto/book.dto.ts
+++ src/app/core/dto/book.dto.ts
@@ -1,7 +1,17 @@
 export class BookDTO {
+	title: string | undefined;
+	author: string | undefined;
+	description: string | undefined;
+
 	constructor(formValue: BookFormValue) {
+		this.title = formValue.title;
+		this.author = formValue.author;
+		this.description = formValue.description;
 	}
 }
 
 export interface BookFormValue {
+	title?: string;
+	author?: string;
+	description?: string;
 }
--- src/app/core/dto/comment.dto.ts
+++ src/app/core/dto/comment.dto.ts
@@ -1,7 +1,11 @@
 export class CommentDTO {
+	content: string | undefined;
+
 	constructor(formValue: CommentFormValue) {
+		this.content = formValue.content; 
 	}
 }
 
 export interface CommentFormValue {
+	content: string;
 }
