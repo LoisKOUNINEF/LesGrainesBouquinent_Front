44d62c1c923cdb64aaa89fe25479c920e8795d60
Lois KOUNINEF <lkouninef@gmail.com>
2023-03-16 12:24:11 +0100

    login method tests

--- src/app/core/services/auth/auth.service.spec.ts
+++ src/app/core/services/auth/auth.service.spec.ts
@@ -1,41 +1,53 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { createMock } from '@golevelup/ts-jest';
+import { MockService } from 'ng-mocks';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { SharedModulesModule } from 'src/app/shared/modules/shared-modules.module';
+import { UserDTO } from '../../dto/user.dto';
 import { ApiCallService } from '../api-call/api-call.service';
 
 import { AuthService } from './auth.service';
+import { User } from '../../models/user.model';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
-
-  let httpMock: HttpTestingController;
+  let apiCallServiceMock = MockService(ApiCallService);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule,
+        SharedModulesModule,
       ],
       providers: [
-      {
-        provide: ApiCallService,
-        useValue: createMock<ApiCallService>(),
-      }
+        {
+          provide: ApiCallService,
+          useValue: apiCallServiceMock,
+        }
       ],
     });
     service = TestBed.inject(AuthService);
-  });
-
-  afterEach(() => {
-    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  // describe('login request', () => {
-  //   it('should login user', () => {
-      
-  //   });
-  // });
+  describe('login request', () => {
+    it('should have a login request', () => {
+      expect(service.login).toBeDefined();
+      expect(service.login).toBeInstanceOf(Function)
+    });
+    it('should call ApiCallService post request', async () => {
+      const userDto: UserDTO = {
+        email: "lolo@yopmail.com",
+        name: "lolo",
+        password: "1234567890"
+      }
+      apiCallServiceMock.post = (jest.fn(() => of(userDto as User))) as any;
+      const loggedIn = await service.login(userDto).toPromise();
+      expect(loggedIn!.name).toBe("lolo");
+      expect(loggedIn!.email).toBe("lolo@yopmail.com");
+      expect(apiCallServiceMock.post).toHaveBeenCalled();
+      expect(apiCallServiceMock).toBeDefined()
+    })
+  });
 });
