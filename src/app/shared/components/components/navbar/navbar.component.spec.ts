7e08fc0e86e98e2b727270bdb49608df9288a513
Lois KOUNINEF <lkouninef@gmail.com>
2023-03-18 10:38:05 +0100

    navbar .ts tests

--- src/app/shared/components/components/navbar/navbar.component.spec.ts
+++ src/app/shared/components/components/navbar/navbar.component.spec.ts
@@ -1,23 +1,60 @@
+import { MockService } from 'ng-mocks';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 import { NavbarComponent } from './navbar.component';
+import { Router } from '@angular/router';
+import { lastValueFrom, of } from 'rxjs';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let authServiceMock = MockService(AuthService);
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        }
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('authServiceMock', () => {
+    it('should be defined', () => {
+      expect(authServiceMock).toBeDefined();
+    });
+  });
+
+  describe('logout method', () => {
+    authServiceMock.logout = (jest.fn(() => of(null)));
+
+    it('should have a logout method', () => {
+      expect(component.logout).toBeDefined();
+      expect(component.logout).toBeInstanceOf(Function);
+    });
+    it('should call Auth Service logout method', async () => {
+      const loggedOut = await lastValueFrom(authServiceMock.logout());
+      expect(authServiceMock.logout).toHaveBeenCalled();
+    });
+    it('should redirect to home page', () => {
+      jest.spyOn(router, 'navigate');
+      component.logout();
+      expect(router.navigate).toHaveBeenCalledWith(['/']);
+    });
   });
 });
--- src/app/shared/components/components/navbar/navbar.component.ts
+++ src/app/shared/components/components/navbar/navbar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  
+  auth: any;  
+  subscription: Subscription = new Subscription();
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+      this.auth = this.authService;
+  }
+
+  logout(): void {
+    this.subscription = this.authService.logout()
+      .subscribe(() => this.router.navigate(['/']));
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
 }
