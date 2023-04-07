0ccaee7873e3fb5aeafb882a6d64ac3957af4ded
Lois KOUNINEF <lkouninef@gmail.com>
2023-04-07 09:40:18 +0200

    findAll users method

--- src/app/core/services/users/users.service.spec.ts
+++ src/app/core/services/users/users.service.spec.ts
@@ -1,16 +1,67 @@
 import { TestBed } from '@angular/core/testing';
+import { MockService } from 'ng-mocks';
+import { lastValueFrom, of } from 'rxjs';
+import { SharedModulesModule } from 'src/app/shared/modules/shared-modules.module';
+import { UserStub } from 'tests/stub-dto/user.stub';
+import { User } from '../../models/user.model';
+import { ApiCallService } from '../api-call/api-call.service';
 
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let apiCallServiceMock = MockService(ApiCallService);
+
+  const userDto = new UserStub;
+  const users = [
+    {
+      name: 'lolo', 
+      email: 'lolo@yopmail.com',
+    },
+    {
+      name: 'lolo2', 
+      email: 'lolo2@yopmail.com',
+    }
+  ];
+  const usersUrl = '/users';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        SharedModulesModule,
+      ],
+      providers: [
+        {
+          provide: ApiCallService,
+          useValue: apiCallServiceMock,
+        }
+      ],
+    });
     service = TestBed.inject(UsersService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('apiCallServiceMock', () => {
+    it('should be defined', () => {
+      expect(apiCallServiceMock).toBeDefined();
+    });
+  });
+
+  describe('findAll method', () => {
+    it('should have a findAll method', () => {
+      expect(service.findAll).toBeDefined();
+      expect(service.findAll).toBeInstanceOf(Function);
+    });
+    it('should call ApiCallService get request', async () => {
+      apiCallServiceMock.get = (jest.fn(() => of(users as User[]))) as any;
+      const usersList = await lastValueFrom(service.findAll());      
+      expect(apiCallServiceMock.get).toHaveBeenCalled();
+      expect(apiCallServiceMock.get).toHaveBeenCalledWith(usersUrl);
+      expect(usersList.length).toBe(2);
+      expect(usersList[0].name).toBe('lolo');
+    });
   });
 });
--- src/app/core/services/users/users.service.ts
+++ src/app/core/services/users/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.model';
+import { ApiCallService } from '../api-call/api-call.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private readonly usersUrl = '/users'
+
+  constructor(private apiCallService: ApiCallService) { }
+
+  findAll(): Observable<User[]> {
+    return this.apiCallService.get(this.usersUrl);
+  }
 }
