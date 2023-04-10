0059bc31f345aa1ec04a844bebc07f8b6e2d36dd
Lois KOUNINEF <lkouninef@gmail.com>
2023-04-10 15:31:27 +0200

    add http to apiUrl (dev env)

--- src/environments/environments/environment.ts
+++ src/environments/environments/environment.ts
@@ -5,7 +5,7 @@ import { Environment } from "../ienvironment";
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'localhost:3000'
+  apiUrl: 'http://localhost:3000'
 };
 
 /*
