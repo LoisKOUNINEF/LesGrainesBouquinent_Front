41b6f1a72bfcd43eeff69afeb62fbb5769c19a91
Lois KOUNINEF <lkouninef@gmail.com>
2023-04-10 16:24:47 +0200

    send pwd reset token component

--- src/app/components/auth/send-reset-link/send-reset-link.component.spec.ts
+++ src/app/components/auth/send-reset-link/send-reset-link.component.spec.ts
@@ -1,23 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { MockService } from 'ng-mocks';
+import { lastValueFrom, of } from 'rxjs';
+import { UserDTO } from 'src/app/core/dto/user.dto';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { FixNavigationTriggeredOutsideAngularZoneNgModule } from 'tests/FixNavigationTriggeredOutsideAngularZoneNgModule';
+import { UserStub } from 'tests/stub-dto/user.stub';
 
 import { SendResetLinkComponent } from './send-reset-link.component';
 
 describe('SendResetLinkComponent', () => {
   let component: SendResetLinkComponent;
   let fixture: ComponentFixture<SendResetLinkComponent>;
+  let authServiceMock = MockService(AuthService);
+  let router: Router;
+  const userDto = new UserStub;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SendResetLinkComponent ]
+      imports: [
+        FixNavigationTriggeredOutsideAngularZoneNgModule,
+      ],
+      declarations: [ SendResetLinkComponent ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        }
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(SendResetLinkComponent);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('authServiceMock', () => {
+    it('should be defined', () => {
+      expect(authServiceMock).toBeDefined();
+    });
+  });
+
+  describe('sendPwdResetLink method', () => {
+    authServiceMock.sendPwdResetLink = (jest.fn(() => of(userDto as UserDTO))) as any;
+
+    it('should have a sendPwdResetLink method', () => {
+      expect(component.sendPwdResetLink).toBeDefined();
+      expect(component.sendPwdResetLink).toBeInstanceOf(Function);
+    });
+    it('should call Auth Service sendPwdResetLink method', async () => {
+      await lastValueFrom(authServiceMock.sendPwdResetLink(userDto));
+      expect(authServiceMock.sendPwdResetLink).toHaveBeenCalled();
+    });
+    it('should redirect to login page', () => {
+      jest.spyOn(router, 'navigate');
+      component.sendPwdResetLink();
+      expect(router.navigate).toHaveBeenCalledWith(['auth/reset-password']);
+    });
   });
 });
--- src/app/components/auth/send-reset-link/send-reset-link.component.ts
+++ src/app/components/auth/send-reset-link/send-reset-link.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { UserDTO, UserFormValue } from 'src/app/core/dto/user.dto';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-send-reset-link',
@@ -6,5 +12,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./send-reset-link.component.css']
 })
 export class SendResetLinkComponent {
+
+  subscription: Subscription = new Subscription;
+
+  pwdResetLinkForm = this.formBuilder.group({
+    email: new FormControl('',[
+      Validators.required,
+      Validators.email
+    ])
+  });
+
+  controls = {
+    email: this.pwdResetLinkForm.get('email'),
+  }
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router) { }
+
+  ngOnInit() { }
+
+  sendPwdResetLink(): Subscription | Promise<boolean> {
+    if (!this.pwdResetLinkForm.valid) {
+      return this.router.navigate(['auth/reset-password']);
+    };
+    const user = new UserDTO(this.pwdResetLinkForm.value as UserFormValue);
+
+    return this.subscription = this.authService.sendPwdResetLink(user)
+      .pipe(filter(res => !!res))
+      .subscribe(() => {
+        this.router.navigate(['auth/reset-link-sent']);
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    if(this.subscription) this.subscription.unsubscribe();
+  }
 
 }
