0998833545de81fad95a6830d331632e4bf74712
Lois KOUNINEF <lkouninef@gmail.com>
2023-04-11 12:01:48 +0200

    reset pwd component

--- src/app/components/auth/reset-password/reset-password.component.spec.ts
+++ src/app/components/auth/reset-password/reset-password.component.spec.ts
@@ -1,18 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockService } from 'ng-mocks';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { FixNavigationTriggeredOutsideAngularZoneNgModule } from 'tests/FixNavigationTriggeredOutsideAngularZoneNgModule';
+import { UserStub } from 'tests/stub-dto/user.stub';
 
 import { ResetPasswordComponent } from './reset-password.component';
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
   let fixture: ComponentFixture<ResetPasswordComponent>;
+  let authServiceMock = MockService(AuthService);
+  let router: Router;
+  const userDto = new UserStub;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResetPasswordComponent ]
+      imports: [
+        FixNavigationTriggeredOutsideAngularZoneNgModule,
+        RouterTestingModule,
+      ],
+      declarations: [ ResetPasswordComponent ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        }
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ResetPasswordComponent);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,4 +41,62 @@ describe('ResetPasswordComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('authServiceMock', () => {
+    it('should be defined', () => {
+      expect(authServiceMock).toBeDefined();
+    });
+  });
+
+  describe('password input field', () => {
+      it('shoud have an input element with class form-class for password field', () => {
+      const el = fixture.debugElement.query(By.css('.form-class.password'));
+      expect(el).toBeTruthy();
+      });
+      it('should have password form attributes', () => {
+        const el = fixture.debugElement.query(By.css('.password input.form-input'))
+        expect(el.nativeElement.getAttribute('type')).toEqual('password');
+        expect(el.nativeElement.getAttribute('id')).toEqual('password');
+        expect(el.nativeElement.getAttribute('class')).toEqual('form-input');
+        expect(el.nativeElement.getAttribute('formControlName')).toEqual('password');
+      });
+      it('should mark password invalid if less than 10 char long', () => {
+        const password = component.pwdResetForm.get('password');
+        password?.setValue('12345678');
+        expect(password?.valid).toBeFalsy();
+      });
+      it('should mark password valid if at least 10 char long', () => {
+        const password = component.pwdResetForm.get('password');
+        password?.setValue('1234567890');
+        expect(password?.valid).toBeTruthy();
+      });
+    });
+
+    describe('confirm password input field', () => {
+      it('shoud have an input element with class form-class for confirm field', () => {
+      const el = fixture.debugElement.query(By.css('.form-class.confirm'));
+      expect(el).toBeTruthy();
+      });
+      it('should have confirm form attributes', () => {
+        const el = fixture.debugElement.query(By.css('.confirm input.form-input'))
+        expect(el.nativeElement.getAttribute('type')).toEqual('password');
+        expect(el.nativeElement.getAttribute('id')).toEqual('confirm');
+        expect(el.nativeElement.getAttribute('class')).toEqual('form-input');
+        expect(el.nativeElement.getAttribute('formControlName')).toEqual('confirmPassword');
+      });
+      it('should mark that passwords do not match', () => {
+        const password = component.pwdResetForm.get('password');
+        const confirm = component.pwdResetForm.get('confirmPassword');
+        password?.setValue('1234567890');
+        confirm?.setValue('0912345678');
+        expect(component.pwdResetForm.valid).toBeFalsy();
+      });
+      it('should be valid if passwords match', () => {
+        const password = component.pwdResetForm.get('password');
+        const confirm = component.pwdResetForm.get('confirmPassword');
+        password?.setValue('1234567890');
+        confirm?.setValue('1234567890');
+        expect(component.pwdResetForm.valid).toBeTruthy();
+      });
+    });
 });
--- src/app/components/auth/reset-password/reset-password.component.ts
+++ src/app/components/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { UserDTO, UserFormValue } from 'src/app/core/dto/user.dto';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { checkPasswords } from 'src/app/shared/helpers/check-passwords';
+import { revealPassword } from 'src/app/shared/helpers/reveal-password';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,4 +15,51 @@ import { Component } from '@angular/core';
 })
 export class ResetPasswordComponent {
 
+  token: string = '';
+  subscription: Subscription = new Subscription;
+  revealPassword = revealPassword;
+  checkPasswords = checkPasswords;
+
+  pwdResetForm = this.formBuilder.group({
+    password: new FormControl('',[
+      Validators.minLength(10)
+    ]),
+    confirmPassword: [''],
+  }, 
+  { 
+    validators: this.checkPasswords,
+  });
+
+  controls = {
+    password: this.pwdResetForm.get('password'),
+    confirmPassword: this.pwdResetForm.get('confirm-password'),
+  }
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    ) { }
+
+  ngOnInit() { this.token = this.route.snapshot.params['token'] }
+
+  resetPwd(): Subscription | Promise<boolean> {
+    if (!this.pwdResetForm.valid) {
+      return this.router.navigate([`auth/reset-password/${this.token}`]);
+    };
+    const user = new UserDTO(this.pwdResetForm.value as UserFormValue);
+      
+    return this.subscription = this.authService.resetPwd(user, this.token)
+      .pipe(filter(res => !!res))
+      .subscribe(() => {
+        this.router.navigate(['auth/login'])
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    if(this.subscription) this.subscription.unsubscribe();
+  }
+  
 }
