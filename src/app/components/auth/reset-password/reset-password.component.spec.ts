301198d890ab15fd991ad3b90e9dfcb067c981d0
Lois KOUNINEF <lkouninef@gmail.com>
2023-04-11 12:08:45 +0200

    remove unused import

--- src/app/components/auth/reset-password/reset-password.component.spec.ts
+++ src/app/components/auth/reset-password/reset-password.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockService } from 'ng-mocks';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
